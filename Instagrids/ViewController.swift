941fc0e2b1303ac0556b2bf0c81ba9ac417af93b
steph29 <fingolfin3529@gmail.com>
2019-01-08 22:32:26 +0100

    Add feature on storyboard.

--- Instagrids/ViewController.swift
+++ Instagrids/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func plusButton(_ sender: Any) {
     }
 
-
-}
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+            }
