2802d223e15d795991209ca2b672e21b6aabfdc7
steph29 <fingolfin3529@gmail.com>
2019-02-06 22:27:16 +0100

    Update of constraints in portrait

--- Instagrids/Controller/ViewController.swift
+++ Instagrids/Controller/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func plusButton(_ sender: Any) {
-    }
+   
+    @IBOutlet weak var squareLowRight: UIImageView!
+    @IBOutlet weak var squareLowLeft: UIImageView!
+    @IBOutlet weak var rectangleHigh: UIImageView!
+    @IBOutlet weak var rectangleLow: UIImageView!
+    @IBOutlet weak var squareHighRight: UIImageView!
+    @IBOutlet weak var squareHighLeft: UIImageView!
+    @IBOutlet weak var checkMarkButtonLeft: UIButton!
+    @IBOutlet weak var checkMarkButtonMiddle: UIButton!
+    @IBOutlet weak var checkMarkButtonRight: UIButton!
+    
 
+ 
+    @IBAction func checkmarkLeft(_ sender: Any) {
+        ItemsHidden(button: checkMarkButtonMiddle, secondButton: checkMarkButtonRight, firstImage: squareHighRight, secondImage: squareHighLeft, thirdImage: rectangleLow)
+       
+    }
+    
+    private func ItemsHidden(button: UIButton, secondButton: UIButton, firstImage: UIImageView, secondImage: UIImageView, thirdImage: UIImageView){
+        button.imageView?.isHidden = true
+        secondButton.imageView?.isHidden = true
+        firstImage.isHidden = true
+        secondImage.isHidden = true
+        thirdImage.isOpaque = true
+    }
+    
+    private func RectangleHidden(rectangle: UIImageView){
+        rectangle.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 143, trailing: 0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         }
